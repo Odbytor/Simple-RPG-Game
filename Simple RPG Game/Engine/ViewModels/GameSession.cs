005dde1ed2fe7cbee938edce15fb661b2584c935
Bartosz Baginski <donmarchewa@gmail.com>
2019-01-04 14:06:59 +0100

    Moving around world added.

--- Simple RPG Game/Engine/ViewModels/GameSession.cs
+++ Simple RPG Game/Engine/ViewModels/GameSession.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,32 @@ using Engine.Factories;
 
 namespace Engine.ViewModels
 {
-    public class GameSession
+    public class GameSession: INotifyPropertyChanged
     {
+        private Location _CurrentLocation;
+
         public World CurrentWorld { get; set; }
         public Player CurrentPlayer { get; set; }
-        public Location CurrentLocation { get; set; }
+        public Location CurrentLocation
+        {
+            get { return _CurrentLocation; }
+            set
+            {
+                _CurrentLocation = value;
+
+                OnPropertyChanged("CurrentLocation");
+                OnPropertyChanged("HasLocationToNorth");
+                OnPropertyChanged("HasLocationToEast");
+                OnPropertyChanged("HasLocationToWest");
+                OnPropertyChanged("HasLocationToSouth");
+            }
+        }
 
         //Commands
-        public RelayCommand AddXPCommand { get; set; }
+        public RelayCommand MoveNorthCommand { get; set; }
+        public RelayCommand MoveSouthCommand { get; set; }
+        public RelayCommand MoveEastCommand { get; set; }
+        public RelayCommand MoveWestCommand { get; set; }
         //Constructor
         public GameSession()
         {
@@ -28,18 +47,79 @@ namespace Engine.ViewModels
             CurrentPlayer.HitPoints = 10;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
-            AddXPCommand = new RelayCommand(AddXP);
-
-           
+            //RelayCommands
+            MoveNorthCommand = new RelayCommand(OnClickMoveNorth);
+            MoveSouthCommand = new RelayCommand(OnClickMoveSouth);
+            MoveEastCommand = new RelayCommand(OnClickMoveEast);
+            MoveWestCommand = new RelayCommand(OnClickMoveWest);
             //World
             WorldFactory factory = new WorldFactory();
             CurrentWorld = factory.CreateWorld();
-            CurrentLocation = CurrentWorld.LocationAt(0, -1);
+            CurrentLocation = CurrentWorld.LocationAt(0, 0);
         }
 
-        private void AddXP(object s)
+        private void OnClickMoveNorth(object s)
         {
-            CurrentPlayer.ExperiencePoints = CurrentPlayer.ExperiencePoints + 10;
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
+        }
+
+        private void OnClickMoveWest(object s)
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate-1, CurrentLocation.YCoordinate);
+        }
+
+        private void OnClickMoveEast(object s)
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate+1, CurrentLocation.YCoordinate);
+        }
+
+        private void OnClickMoveSouth(object s)
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+        }
+
+        //InotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        //Checking visibilty
+
+        public bool HasLocationToNorth
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1) != null;
+            }
+        }
+
+        public bool HasLocationToEast
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate) != null;
+            }
+        }
+
+        public bool HasLocationToSouth
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1) != null;
+            }
+        }
+
+        public bool HasLocationToWest
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate) != null;
+            }
         }
 
     }
